3d402d159853675df6daeeff449128d69958d0b0
Cyber Oliveira <lccro@msn.com>
2022-09-27 13:13:10 -0300

    upgrades

--- cmd/quicklogs/main.go
+++ cmd/quicklogs/main.go
@@ -1,202 +1,201 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"log"
-	"sort"
+  "context"
+  "fmt"
+  "io"
+  "log"
+  "sort"
 
-	"github.com/gdamore/tcell"
-
-	"github.com/docker/cli/cli/compose/convert"
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/filters"
-	"github.com/docker/docker/api/types/swarm"
-	"github.com/docker/docker/client"
-	"github.com/rivo/tview"
+  "github.com/docker/cli/cli/compose/convert"
+  "github.com/docker/docker/api/types"
+  "github.com/docker/docker/api/types/filters"
+  "github.com/docker/docker/api/types/swarm"
+  "github.com/docker/docker/client"
+  "github.com/gdamore/tcell/v2"
+  "github.com/rivo/tview"
 )
 
 var (
-	app    *tview.Application
-	cancel context.CancelFunc
-	cli    *client.Client
+  app    *tview.Application
+  cancel context.CancelFunc
+  cli    *client.Client
 )
 
 func init() {
-	var err error
-	cli, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		log.Fatalln("unable to create docker client:", err)
-	}
-	app = tview.NewApplication()
+  var err error
+  cli, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+  if err != nil {
+    log.Fatalln("unable to create docker client:", err)
+  }
+  app = tview.NewApplication()
 }
 
 func main() {
-	if err := browser(); err != nil {
-		log.Fatalln("browser error:", err)
-	}
-	if err := app.Run(); err != nil {
-		log.Fatalln("app.Run error:", err)
-	}
+  if err := browser(); err != nil {
+    log.Fatalln("browser error:", err)
+  }
+  if err := app.Run(); err != nil {
+    log.Fatalln("app.Run error:", err)
+  }
 }
 
 func browser() error {
-	// Create the basic objects.
-	lstStacks := tview.
-		NewList().
-		ShowSecondaryText(false).
-		SetDoneFunc(func() { app.Stop() })
-	lstStacks.
-		SetBorder(true).
-		SetTitle("Stacks")
+  // Create the basic objects.
+  lstStacks := tview.
+    NewList().
+    ShowSecondaryText(false).
+    SetDoneFunc(func() { app.Stop() })
+  lstStacks.
+    SetBorder(true).
+    SetTitle("Stacks")
 
-	lstServices := tview.
-		NewList().
-		ShowSecondaryText(false).
-		SetSelectedFocusOnly(true).
-		SetDoneFunc(func() { app.Stop() })
-	lstServices.
-		SetBorder(true).
-		SetTitle("Services")
+  lstServices := tview.
+    NewList().
+    ShowSecondaryText(false).
+    SetSelectedFocusOnly(true).
+    SetDoneFunc(func() { app.Stop() })
+  lstServices.
+    SetBorder(true).
+    SetTitle("Services")
 
-	txtLogs := tview.
-		NewTextView().
-		SetDynamicColors(true).
-		SetChangedFunc(func() { app.Draw() })
-	txtLogs.
-		SetBorder(true).
-		SetTitle("tail -f (starting 10min ago)")
+  txtLogs := tview.
+    NewTextView().
+    SetDynamicColors(true).
+    SetChangedFunc(func() { app.Draw() })
+  txtLogs.
+    SetBorder(true).
+    SetTitle("tail -f (starting 10min ago)")
 
-	// set up key events
-	lstStacks.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyRight || event.Key() == tcell.KeyEnter {
-			app.SetFocus(lstServices)
-			return nil
-		}
-		return event
-	})
+  // set up key events
+  lstStacks.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+    if event.Key() == tcell.KeyRight || event.Key() == tcell.KeyEnter {
+      app.SetFocus(lstServices)
+      return nil
+    }
+    return event
+  })
 
-	lstServices.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyLeft ||
-			event.Key() == tcell.KeyESC {
-			app.SetFocus(lstStacks)
-			return nil
-		}
-		if event.Key() == tcell.KeyEnter {
-			app.SetFocus(txtLogs)
-			return nil
-		}
-		return event
-	})
+  lstServices.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+    if event.Key() == tcell.KeyLeft ||
+      event.Key() == tcell.KeyESC {
+      app.SetFocus(lstStacks)
+      return nil
+    }
+    if event.Key() == tcell.KeyEnter {
+      app.SetFocus(txtLogs)
+      return nil
+    }
+    return event
+  })
 
-	txtLogs.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyESC {
-			app.SetFocus(lstServices)
-			return nil
-		}
-		return event
-	})
+  txtLogs.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+    if event.Key() == tcell.KeyESC {
+      app.SetFocus(lstServices)
+      return nil
+    }
+    return event
+  })
 
-	// Create the layout.
-	top := tview.NewFlex().
-		AddItem(lstStacks, 0, 1, true).
-		AddItem(lstServices, 0, 4, false)
-	pages := tview.NewFlex().
-		SetDirection(tview.FlexRow).
-		AddItem(top, 0, 1, true).
-		AddItem(txtLogs, 0, 4, false)
+  // Create the layout.
+  top := tview.NewFlex().
+    AddItem(lstStacks, 0, 1, true).
+    AddItem(lstServices, 0, 4, false)
+  pages := tview.NewFlex().
+    SetDirection(tview.FlexRow).
+    AddItem(top, 0, 1, true).
+    AddItem(txtLogs, 0, 4, false)
 
-	// create map: stackName -> [service1, service2, ...]
-	stackNames, stackServices, err := extractStackAndServices()
-	if err != nil {
-		return err
-	}
-	if len(stackNames) == 0 {
-		return fmt.Errorf("no docker swarm stacks found")
-	}
+  // create map: stackName -> [service1, service2, ...]
+  stackNames, stackServices, err := extractStackAndServices()
+  if err != nil {
+    return err
+  }
+  if len(stackNames) == 0 {
+    return fmt.Errorf("no docker swarm stacks found")
+  }
 
-	containerLogsOptions := types.ContainerLogsOptions{
-		ShowStdout: true,
-		ShowStderr: true,
-		Timestamps: true,
-		Follow:     true,
-		Since:      "10m",
-	}
+  containerLogsOptions := types.ContainerLogsOptions{
+    ShowStdout: true,
+    ShowStderr: true,
+    Timestamps: true,
+    Follow:     true,
+    Since:      "10m",
+  }
 
-	// When the user selects a stack, show its services.
-	for _, stackName := range stackNames {
-		lstStacks.AddItem(stackName, "", 0, nil)
-	}
-	lstStacks.SetChangedFunc(func(i int, stackName string, secondaryLabel string, s rune) {
-		lstServices.Clear()
-		// adds services of this stack
-		for _, serviceName := range stackServices[stackName] {
-			lstServices.AddItem(serviceName, "", 0, nil)
-		}
-	})
-	// force change event
-	lstStacks.SetCurrentItem(-1)
-	lstStacks.SetCurrentItem(0)
+  // When the user selects a stack, show its services.
+  for _, stackName := range stackNames {
+    lstStacks.AddItem(stackName, "", 0, nil)
+  }
+  lstStacks.SetChangedFunc(func(i int, stackName string, secondaryLabel string, s rune) {
+    lstServices.Clear()
+    // adds services of this stack
+    for _, serviceName := range stackServices[stackName] {
+      lstServices.AddItem(serviceName, "", 0, nil)
+    }
+  })
+  // force change event
+  lstStacks.SetCurrentItem(-1)
+  lstStacks.SetCurrentItem(0)
 
-	// When the user selects a service, show its log.
-	lstServices.SetChangedFunc(func(_ int, serviceName string, _ string, _ rune) {
-		if cancel != nil {
-			cancel()
-		}
-		txtLogs.Clear()
+  // When the user selects a service, show its log.
+  lstServices.SetChangedFunc(func(_ int, serviceName string, _ string, _ rune) {
+    if cancel != nil {
+      cancel()
+    }
+    txtLogs.Clear()
 
-		var ctx context.Context
-		ctx, cancel = context.WithCancel(context.Background())
+    var ctx context.Context
+    ctx, cancel = context.WithCancel(context.Background())
 
-		logStream, err := cli.ServiceLogs(ctx, serviceName, containerLogsOptions)
-		if err != nil {
-			errMsg := fmt.Sprintf("error opening logs for service %s: %v", serviceName, err)
-			_, _ = txtLogs.Write([]byte(errMsg))
-			return
-		}
+    logStream, err := cli.ServiceLogs(ctx, serviceName, containerLogsOptions)
+    if err != nil {
+      errMsg := fmt.Sprintf("error opening logs for service %s: %v", serviceName, err)
+      _, _ = txtLogs.Write([]byte(errMsg))
+      return
+    }
 
-		go io.Copy(tview.ANSIWriter(txtLogs), logStream)
-	})
-	// force change event
-	lstServices.SetCurrentItem(-1)
-	lstServices.SetCurrentItem(0)
+    go io.Copy(tview.ANSIWriter(txtLogs), logStream)
+  })
+  // force change event
+  lstServices.SetCurrentItem(-1)
+  lstServices.SetCurrentItem(0)
 
-	app.SetRoot(pages, true)
-	return nil
+  app.SetRoot(pages, true)
+  return nil
 }
 
 func extractStackAndServices() (stackNames []string, stackServices map[string][]string, err error) {
-	// list swarm services
-	var allServices []swarm.Service
-	allServices, err = cli.ServiceList(
-		context.Background(),
-		types.ServiceListOptions{
-			Filters: filters.NewArgs(filters.Arg("label", "com.docker.stack.namespace"))})
-	if err != nil {
-		return
-	}
-	// foreach swarm service
-	stackServices = make(map[string][]string)
-	for _, service := range allServices {
-		stackName, ok := service.Spec.Labels[convert.LabelNamespace]
-		if !ok {
-			err = fmt.Errorf("cannot get label %s for service %s", convert.LabelNamespace, service.ID)
-			return
-		}
-		// if it's a new stack name, keep it
-		_, ok = stackServices[stackName]
-		if !ok {
-			stackNames = append(stackNames, stackName)
-		}
-		// save service name on stack => services map
-		stackServices[stackName] = append(stackServices[stackName], service.Spec.Name)
-	}
-	// sort service names
-	for _, services := range stackServices {
-		sort.Strings(services)
-	}
-	// sort stack names
-	sort.Strings(stackNames)
-	return stackNames, stackServices, err
+  // list swarm services
+  var allServices []swarm.Service
+  allServices, err = cli.ServiceList(
+    context.Background(),
+    types.ServiceListOptions{
+      Filters: filters.NewArgs(filters.Arg("label", "com.docker.stack.namespace"))})
+  if err != nil {
+    return
+  }
+  // foreach swarm service
+  stackServices = make(map[string][]string)
+  for _, service := range allServices {
+    stackName, ok := service.Spec.Labels[convert.LabelNamespace]
+    if !ok {
+      err = fmt.Errorf("cannot get label %s for service %s", convert.LabelNamespace, service.ID)
+      return
+    }
+    // if it's a new stack name, keep it
+    _, ok = stackServices[stackName]
+    if !ok {
+      stackNames = append(stackNames, stackName)
+    }
+    // save service name on stack => services map
+    stackServices[stackName] = append(stackServices[stackName], service.Spec.Name)
+  }
+  // sort service names
+  for _, services := range stackServices {
+    sort.Strings(services)
+  }
+  // sort stack names
+  sort.Strings(stackNames)
+  return stackNames, stackServices, err
 }
--- go.mod
+++ go.mod
@@ -1,98 +1,80 @@
 module github.com/lccro/quicklogs
 
-go 1.17
+go 1.19
 
 require (
-	github.com/docker/cli v0.0.0-20190824002126-058a4af0bdb4
-	github.com/docker/docker v0.7.3-0.20190825014646-cd1356d9ea63
+	github.com/docker/cli v20.10.18+incompatible
+	github.com/docker/docker v20.10.18+incompatible
 	github.com/gdamore/tcell v1.4.0
-	github.com/rivo/tview v0.0.0-20190721135419-23dc8a0944e4
+	github.com/rivo/tview v0.0.0-20220916081518-2e69b7385a37
 )
 
-require github.com/docker/distribution v2.7.1+incompatible // indirect
+require github.com/docker/distribution v2.8.1+incompatible // indirect
 
 require (
-	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
-	github.com/Microsoft/go-winio v0.5.1 // indirect
-	github.com/Microsoft/hcsshim v0.8.23 // indirect
-	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/Microsoft/hcsshim v0.9.4 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/bits-and-blooms/bitset v1.2.0 // indirect
 	github.com/bugsnag/bugsnag-go v1.5.3 // indirect
 	github.com/bugsnag/panicwrap v1.2.0 // indirect
-	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/cloudflare/cfssl v0.0.0-20190808011637-b1ec8c586c2a // indirect
-	github.com/containerd/cgroups v1.0.1 // indirect
-	github.com/containerd/containerd v1.5.9 // indirect
-	github.com/containerd/continuity v0.2.2 // indirect
-	github.com/containerd/fifo v1.0.0 // indirect
-	github.com/containerd/ttrpc v1.1.0 // indirect
-	github.com/containerd/typeurl v1.0.2 // indirect
-	github.com/coreos/etcd v3.3.10+incompatible // indirect
-	github.com/docker/docker-credential-helpers v0.6.3 // indirect
-	github.com/docker/go v1.5.1-1 // indirect
+	github.com/containerd/continuity v0.3.0 // indirect
+	github.com/coreos/etcd v3.3.27+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.7.0 // indirect
+	github.com/docker/go v1.5.1-1.0.20160303222718-d30aec9fd63c // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
 	github.com/docker/go-metrics v0.0.1 // indirect
-	github.com/docker/go-units v0.4.0 // indirect
-	github.com/docker/libtrust v0.0.0-20160708172513-aabc10ec26b7 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/docker/swarmkit v1.12.1-0.20190820142245-958d149179db // indirect
+	github.com/fvbommel/sortorder v1.0.2 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.5.3 // indirect
 	github.com/go-sql-driver/mysql v1.4.1 // indirect
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
-	github.com/gogo/googleapis v1.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/certificate-transparency-go v1.0.21 // indirect
-	github.com/google/shlex v0.0.0-20181106134648-c34317bd91bf // indirect
-	github.com/google/uuid v1.2.0 // indirect
-	github.com/gorilla/mux v1.7.3 // indirect
-	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
-	github.com/hashicorp/go-version v1.2.0 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/jinzhu/gorm v1.9.10 // indirect
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
-	github.com/klauspost/compress v1.11.13 // indirect
-	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
-	github.com/mattn/go-runewidth v0.0.7 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
-	github.com/miekg/pkcs11 v1.0.3 // indirect
-	github.com/moby/locker v1.0.1 // indirect
-	github.com/moby/sys/mountinfo v0.4.1 // indirect
-	github.com/morikuni/aec v0.0.0-20170113033406-39771216ff4c // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2 // indirect
+	github.com/miekg/pkcs11 v1.1.1 // indirect
+	github.com/moby/sys/mount v0.3.3 // indirect
+	github.com/moby/sys/mountinfo v0.6.2 // indirect
+	github.com/moby/term v0.0.0-20220808134915-39b0c02b01ae // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.0.2 // indirect
-	github.com/opencontainers/runc v1.0.2 // indirect
-	github.com/opencontainers/runtime-spec v1.0.3-0.20210326190908-1c3f411f0417 // indirect
-	github.com/opencontainers/selinux v1.8.2 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc1 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.7.1 // indirect
+	github.com/prometheus/client_golang v1.13.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.10.0 // indirect
-	github.com/prometheus/procfs v0.6.0 // indirect
-	github.com/rivo/uniseg v0.0.0-20190513083848-b9f5b9457d44 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/spf13/cobra v1.0.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/cobra v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/theupdateframework/notary v0.6.1 // indirect
-	github.com/xlab/handysort v0.0.0-20150421192137-fb3537ed64a1 // indirect
+	github.com/theupdateframework/notary v0.7.0 // indirect
 	github.com/zmap/zcrypto v0.0.0-20190729165852-9051775e6a2e // indirect
 	github.com/zmap/zlint v1.0.1 // indirect
-	go.opencensus.io v0.22.3 // indirect
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
-	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
-	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
-	golang.org/x/text v0.3.4 // indirect
-	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
-	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a // indirect
-	google.golang.org/grpc v1.33.2 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
-	gopkg.in/dancannon/gorethink.v3 v3.0.5 // indirect
-	gopkg.in/fatih/pool.v2 v2.0.0 // indirect
-	gopkg.in/gorethink/gorethink.v3 v3.0.5 // indirect
-	vbom.ml/util v0.0.0-20180919145318-efcd4e0f9787 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20220927155233-aa73b2587036 // indirect
+	golang.org/x/sys v0.0.0-20220926163933-8cfa568d3c25 // indirect
+	golang.org/x/term v0.0.0-20220919170432-7a66f970e087 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220927151529-dcaddaf36704 // indirect
+	google.golang.org/grpc v1.49.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
